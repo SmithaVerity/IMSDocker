d1e6fe1a6e79f4188aed31bae1fb495b6fcba0d0
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-26 00:18:14 +0530

    Update deployDockerandApp.sh

--- deployDockerandApp.sh
+++ deployDockerandApp.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt install docker.io --yes
 sudo docker network create ims-net
 
 # launch mysql 5.7 container
-sudo docker run -ti --name imsdb --network ims-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
+sudo docker run -ti --name imsdb --network ims-net -e MYSQL_ROOT_PASSWORD=admin -p 3306:3306 -d mysql:5.7
 
 # launch prestashop container
 sudo docker run -ti --name ims --network ims-net -e DB_SERVER=imsdb -p 80:80 -d aspendigital/codeigniter:latest
