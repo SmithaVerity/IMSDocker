9456251d776c1493e5c0d284597bbd55b2a2c285
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-25 23:57:27 +0530

    Update deployDockerandApp.sh

--- deployDockerandApp.sh
+++ deployDockerandApp.sh
@@ -2,4 +2,3 @@ sudo apt update && sudo apt install docker.io --yes
 sudo docker pull aspendigital/codeigniter
 sudo docker run --name ims -p 80:80  -d aspendigital/codeigniter:latest
 sudo docker exec -it ims /bin/bash
-git clone https://github.com/SmithaVerity/IMS.git
