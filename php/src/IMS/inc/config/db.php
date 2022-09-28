97b740a42d21ce3eadb5b68d94f22e40957a33b4
SmithaVerity <smitha@theveritycorp.com>
2022-09-29 00:37:31 +0530

    Update db.php

--- php/src/IMS/inc/config/db.php
+++ php/src/IMS/inc/config/db.php
@@ -1,11 +1,11 @@
 <?php
 	// Connect to database
-	// try{
-		$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} else {
-			echo "Connected to MySQL server successfully!";
-		}
-	
+	try{
+		$conn = new PDO(DSN, DB_USER, DB_PASSWORD);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		$errorMessage = $e->getMessage();
+		echo $errorMessage;
+		exit();
+	}
 ?>
