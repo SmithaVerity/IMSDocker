8bd6600b974374a82ca05310c303db39887c979a
SmithaVerity <smitha@theveritycorp.com>
2022-09-29 00:33:59 +0530

    Update db.php

--- php/src/IMS/inc/config/db.php
+++ php/src/IMS/inc/config/db.php
@@ -1,11 +1,11 @@
 <?php
 	// Connect to database
-	try{
+	// try{
 		$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		} else {
 			echo "Connected to MySQL server successfully!";
 		}
-
+	
 ?>
